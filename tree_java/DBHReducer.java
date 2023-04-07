edd78812d90defa81883adac61bfdca2b060f197
Charlie Cai <lc4181@nyu.edu>
2023-04-07 13:24:03 -0400

    Update DBHReducer.java

--- tree_java/DBHReducer.java
+++ tree_java/DBHReducer.java
@@ -21,23 +21,3 @@ public class DBHReducer extends Reducer<Text, IntWritable, Text, FloatWritable> 
         context.write(key, new FloatWritable(avg));
     }
 }
-
-// from ArrayStack import ArrayStack;
-
-// class queue:
-//     def __init__():
-//         self.stack1 = ArrayStack()
-//         self.stack2 = ArrayStack()
-
-//     def push(value):
-//         self.stack1.push(value);
-
-//     def pop():
-//         number_of_pop = len(self.stack1) - 1
-//         for i in range(number_of_pop):
-//             self.stack2.push(self.stack1.pop())
-//         res = self.stack1.pop()
-//         for i in range(number_of_pop):
-//             self.stack1.push(self.stack2.pop())
-        
-//         return res
