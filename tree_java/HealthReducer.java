c19c95bb6d645687aae7c659b72f85c07adf13a0
Charlie Cai <lc4181@nyu.edu>
2023-04-06 21:17:29 -0400

    commit

---
+++ tree_java/HealthReducer.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class CountGoodRecordsReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        int fair = 0;
+        int good = 0;
+        int poor = 0;
+        
+        for (Text value : values) {
+            if (value.equals("Fair")) {
+                fair += 1;
+            }
+            if (value.equals("Good")) {
+                good += 1;
+            }
+            if (value.equals("Poor")) {
+                poor += 1;
+            }
+        }
+
+        String res = String.format("Fair: %d, Good: %d, Poor: %d", fair, good, poor);
+        
+        // Write the total count to reducer 
+        context.write(key, new Text(res));
+    }
+}
